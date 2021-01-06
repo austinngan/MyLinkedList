5f47e3b0c8951a949fd2663c80a42853fae8b1c4
austinngan <angan20@stuy.edu>
2021-01-06 14:42:03 -0500

    added add and size methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,11 +1,30 @@
 public class MyLinkedList{
   private int size;
   private Node start,end;
+
   public MyLinkedList(){
     size=0;
   }
-  // public int size();
-  // public boolean add(String value);
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String value){
+    if (size==0){
+      start=new Node(value);
+      end=start;
+      size++;
+    }
+    else{
+      Node prev=end;
+      end=new Node(value);
+      prev.setNext(end);
+      end.setPrev(prev);
+      size++;
+    }
+    return true;
+  }
   // public boolean add(int index, String value);
   // public String get(int index);
   // public String set(int index, String value);
